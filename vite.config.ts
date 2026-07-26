839c5a10b7b4a92e4c3fbfbe0d511756950dc25b
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2026-07-26 16:39:56 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          motion: ['framer-motion'],
+        },
+      },
+    },
+  },
+})
